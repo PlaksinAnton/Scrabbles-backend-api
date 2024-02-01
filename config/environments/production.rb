ba57d34927b47be9a6ca2b8ba377d477b95f6e79
Anton Plaksin <aplaksin2000@gmail.com>
2024-02-01 19:14:03 +0400

    Minor change to production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Disable sqlite warning in log.
+  config.active_record.sqlite3_production_warning=false
 end
